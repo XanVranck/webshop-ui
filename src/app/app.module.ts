df8d227d96f64a4d18d1846e6cbf7009f1c09b97
Xan Vranck <vranckaert.xan@gmail.com>
2018-01-03 19:49:25 +0100

    added a login component, just the lay out, nothing else

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {routing} from './app.routing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatGridListModule,
+  MatInputModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatFormFieldModule
+} from '@angular/material';
+import 'hammerjs';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './component/login/login.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './component/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
